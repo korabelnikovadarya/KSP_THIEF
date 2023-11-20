1a69eb62d85fe7da0b9f66a2780e79d6f5896bd8
korabelniikova <korabelnikovadarya2404@mail.ru>
2023-11-20 19:21:13 +0300

    Update Data1.py
    now

--- Data1.py
+++ Data1.py
@@ -1,6 +1,11 @@
 def main():
     print('Welcome to our team!')
     print('мяу мяу')
+    print('мяу мяу')
+    print('мяу мяу')
+    print('мяу мяу')
+    print('мяу мяу')
+
     print('МЯУУУУУУУУ')
 if __name__ == '__main__':
     main()
