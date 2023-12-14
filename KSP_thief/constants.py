e7ce91ab9bb5ff6d87c88beaf47092f7dbc3622c
korabelniikova <korabelnikovadarya2404@mail.ru>
2023-12-14 10:07:04 +0300

    Rename

---
+++ KSP_thief/constants.py
@@ -0,0 +1,125 @@
+import numpy as np
+from barriers import *
+import pygame
+
+#константы
+FPS = 30 # Частота обновления кадров (30 к/с)
+
+# Описываем цвета RGB-схемы
+white = (255, 255, 255)
+black = (0, 0, 0)
+red = (255, 0, 0)
+green = (0, 255, 0)
+floor = (254, 251, 232)
+
+#цвета игры
+d_blue = (100, 131, 249)
+l_blue = (201, 232, 235)
+d_red = (234, 73, 85)
+l_red = (255, 222, 197)
+d_green = (78, 164, 120)
+l_green = (203, 255, 169)
+yellow = (246, 230, 107)
+grey = (129, 129, 129)
+
+
+
+""" наборы мест и их активность """
+
+
+top_y_table = 392
+
+second_row_x = 105
+second_row_y = 570
+table_small_height = 25
+second_row_long = 695
+
+
+table_rect_width = 43 # Ширина стола
+table_height = 80 # Высота стола
+
+
+#верхние и нижние места
+upper_y = 405
+lower_y = 460
+
+left_x_table = 160 # Левый край левого стола по ОХ
+
+#расстояние между стульями по обе стороны от одного стола
+table_width = 68
+
+#расстояние между стульями в проходе
+table_gap = 35
+
+#количество столов
+n_tables = 5
+
+x_table_coord = []
+# Координаты для столов (чтобы охранник обходил эти столы)
+# дальше этого вправо не идет:
+tables_left_coords = [left_x_table + i * (table_width + table_gap) for i in range(5)]
+
+# Координаты для столов нижнего ряда
+tables_left_coords_2 = [second_row_x + i * (table_width + table_gap) for i in range(7)]
+
+
+
+for i in range(n_tables):
+    # левое место у i стола
+    x_table_coord.append(left_x_table - 10  + i * (table_width + table_gap))
+    # правое место у i стола
+    x_table_coord.append(left_x_table - 15 + i * (table_width + table_gap) + table_width)
+
+
+# y-координаты коридоров
+coridor1 = 200
+coridor2 = 345
+coridor3 = 510
+
+# правила игры
+rules_size = 30
+line_pos_x = 5
+line_pos_y = 5
+leng = 16
+
+# генерация студента каждые 2 секунды (в среднем)
+prob_stud = 1 / (FPS * 0.5)
+
+# доля честных студентов
+prob_not_thief = 0.5
+
+# границы поля
+WIDTH = 800
+HEIGHT = 600
+
+# границы слева, справа и снизу
+
+RIGHT = 10
+LEFT = 10
+BOTTOM = 3
+
+# ширина ленты выдачи
+TOP = 123
+
+# координата кассы
+
+PAY_DESK = WIDTH - 90
+
+
+
+# расстояние между студентами
+DS = 5
+
+# координаты охранника в начале игры
+x1 = LEFT
+y1 = TOP + (HEIGHT - BOTTOM - TOP) / 2
+
+# время оплаты
+pay_time = 1 * FPS
+
+# время еды
+eat_time = 2 * FPS
+
+# Количество жизней охранника
+live = 110
+
---
+++ KSP_thief/students.py
@@ -0,0 +1,330 @@
+from pictures import *
+from constants import *
+import pygame
+from functions import *
+from math import sin, cos, pi # для часов
+
+class Student():
+    def __init__(self, window: pygame.Surface):
+        self.window = window
+        self.money = 1
+        self.number_table = 0 # будем запоминать индекс стола, чтобы потом этот стол освобождать
+        self.v = 5
+        #self.v = 0.2
+        self.r = student_r
+        self.x = -2*self.r
+        self.y = 78
+
+        #направление движения студента для правильной отрисовки
+        self.direction = 'r' # right, left, down, up
+
+        self.state = 0
+        # что делает студент
+        # 0 - идет вдоль ленты
+        # 1 - расплачивается
+        # 2 - отходит от кассы
+        # 3 - идет к столу
+        # 4 - ест
+        # 5 - уходит
+        # 6 -
+
+        self.touch = 0 #касаются ли охранник и студент
+        self.minus_life = 1 # переменная, которая обеспечивает только одну -жизнь при касании зеленого
+
+
+        self.color = green
+
+        # время оплаты + время отхода от кассы
+
+        self.time_goaway = (2 * self.r + DS) // self.v
+        self.pay_time = pay_time + self.time_goaway
+        self.eat_time = eat_time
+
+    def move(self,security, students, upper_active, lower_active):
+        if self.state == 0:
+            self.x += self.v
+            
+            #eсли студент воткнулся в студента, то он стоит на месте
+            stud_stud_collide(self, students)
+
+            if self.x >= PAY_DESK:
+                self.state = 1
+            return
+        if self.state == 1:
+            if self.pay_time > 0:
+                self.pay_time -= 1
+            else:
+                if self.state <= 1:
+
+                    # студент выбирает место
+                    if decision(0.5) and any(upper_active):
+                        # c вероятносью 0.5 выбираем верхний ряд
+                        self.state = 3
+
+                        # находим все индексы свободных мест сверху
+                        idx_free, = np.nonzero(upper_active)
+                        # из свободных выбираем одно
+                        idx_table = np.random.choice(idx_free)
+                        # направление взгляда когда будет сидеть за столом
+                        direction = 'r' if idx_table % 2 == 0 else 'l'
+
+                        self.table = (0, x_table_coord[idx_table], direction, idx_table)
+                        # 0 - верхний стол, координата места по x, направление взгляда, индекс стола
+
+                        # делаем место неактивным
+                        upper_active[idx_table] = 0
+
+                    elif any(lower_active):
+                        # c вероятносью 0.5 выбираем нижний ряд
+                        self.state = 3
+
+                        idx_free, = np.nonzero(lower_active)
+                        idx_table = np.random.choice(idx_free)
+                        direction = 'r' if idx_table % 2 == 0 else 'l'
+                        self.table = (1, x_table_coord[idx_table], direction, idx_table)
+                        # 1 - нижний стол, координата места по x, направление взгляда, индекс стола
+                        lower_active[idx_table] = 0
+                    #else:
+                        # если стол не выбран, то чел просто стоит
+                     #   pass
+        """
+        if self.state == 2:
+            if self.time_goaway > 1:
+                self.direction = 'd'
+                self.y += self.v  
+                # если студент воткнулся в охранника или студента, то он стоит на месте
+                if stud_sec_collide(self, security):
+                    self.touch = 1
+                else:
+                    self.touch = 0
+                    self.time_goaway -= 1
+            elif self.time_goaway == 1:
+                self.y += self.v
+                # если студент воткнулся в охранника, то он стоит на месте
+                if stud_sec_collide(self, security):
+                    self.touch = 1
+                else:
+                    self.touch = 0
+                    self.state = 3
+        """
+        if self.state == 3:
+
+            if self.table[0] == 0:
+                # движение к верхнему столу
+                if self.y < coridor2:
+                    self.direction = 'd'
+                    self.y += self.v 
+                    if stud_sec_collide(self, security):
+                        self.touch = 1
+                    else:
+                        self.touch = 0
+                    stud_stud_collide(self, students)
+                    if self.y >= coridor2:
+                        self.y = coridor2
+
+                elif self.x > self.table[1]:
+                    self.direction = 'l'
+                    self.x -= self.v
+                    if stud_sec_collide(self, security):
+                        self.touch = 1
+                    else:
+                        self.touch = 0
+                    stud_stud_collide(self, students)
+                    if self.x <= self.table[1]:
+                        self.x = self.table[1]
+
+                elif self.y < upper_y:
+                    self.direction = 'd'
+                    self.y += self.v 
+                    if stud_sec_collide(self, security):
+                        self.touch = 1
+                    else:
+                        self.touch = 0
+                    stud_stud_collide(self, students)
+                    if self.y >= upper_y:
+                        self.y = upper_y
+                        self.state = 4
+                        self.direction = self.table[2]
+
+
+            else:
+                # движение к нижнему столу
+                if self.y < coridor3 and self.x > self.table[1]:
+                    self.direction = 'd'
+                    self.y += self.v 
+                    if stud_sec_collide(self, security):
+                        self.touch = 1
+                    else:
+                        self.touch = 0
+                    stud_stud_collide(self, students)
+                    if self.y >= coridor3:
+                        self.y = coridor3
+                    
+                elif self.x > self.table[1]:
+                    self.direction = 'l'
+                    self.x -= self.v
+                    if stud_sec_collide(self, security):
+                        self.touch = 1
+                    else:
+                        self.touch = 0
+                    stud_stud_collide(self, students)
+                    if self.x <= self.table[1]:
+                        self.x = self.table[1]
+
+                elif self.y > lower_y:
+                    self.direction = 'u'
+                    self.y -= self.v 
+                    if stud_sec_collide(self, security):
+                        self.touch = 1
+                    else:
+                        self.touch = 0
+                    stud_stud_collide(self, students)
+                    if self.y <= lower_y:
+                        self.y = lower_y
+                        self.state = 4
+                        self.direction = self.table[2]
+
+# self.state = 4 - кушает (прописано в eat)
+
+        if self.state == 5:
+            # студент уходит
+            if self.x < -2*self.r: # удаляется с поля, если ушел
+                self.state = 6
+
+            if self.table[0] == 0:
+                # движение на выход от верхнего стола
+                if self.y >= coridor2:
+                    self.direction = 'u'
+                    self.y -= self.v
+                    if stud_sec_collide(self, security):
+                        self.touch = 1
+                    else:
+                        self.touch = 0
+                    stud_stud_collide(self, students)
+                    if self.y <= coridor2:
+                        self.y = coridor2 - 1
+                else:
+                    self.direction = 'l'
+                    self.x -= self.v
+                    if stud_sec_collide(self, security):
+                        self.touch = 1
+                    else:
+                        self.touch = 0
+                    stud_stud_collide(self, students)
+
+
+
+            else:
+                # движение на выход от нижнего стола
+                if self.y < coridor3:
+                    self.direction = 'd'
+                    self.y += self.v
+                    if stud_sec_collide(self, security):
+                        self.touch = 1
+                    else:
+                        self.touch = 0
+                    stud_stud_collide(self, students)
+                    if self.y >= coridor3:
+                        self.y = coridor3
+                else:
+                    self.direction = 'l'
+                    self.x -= self.v
+                    if stud_sec_collide(self, security):
+                        self.touch = 1
+                    else:
+                        self.touch = 0
+                    stud_stud_collide(self, students)
+
+    def pay(self):
+        if self.state == 1:
+            dollar.set_alpha(255 * self.pay_time // pay_time)
+            dollar_rect.center = self.x, self.y - (pay_time - self.pay_time)
+            self.window.blit(dollar, dollar_rect)
+
+    def eat(self, upper_active, lower_active):
+        # часы, когда кушает
+        if self.state == 4:
+            if self.eat_time > 0:
+                self.eat_time -= 1
+
+                draw_clock(self.window, self.x, self.y - 10, self.eat_time)
+            else:
+                self.state = 5
+                if self.table[0] == 0:
+                    upper_active[self.table[3]] = 1
+                if self.table[0] == 1:
+                    lower_active[self.table[3]] = 1
+
+
+    def draw(self):
+        if self.direction == 'r':
+            angle = 0
+        elif self.direction == 'l':
+            angle = 180
+        elif self.direction == 'd':
+            angle = 270
+        elif self.direction == 'u':
+            angle = 90
+        
+        if self.state == 0:
+            pic = pygame.transform.rotate(stud_blue, angle)
+            stud_blue_rect.center = self.x, self.y
+            self.window.blit(pic, stud_blue_rect)
+        else:
+            pic = pygame.transform.rotate(stud_green, angle)
+            stud_green_rect.center = self.x, self.y
+            self.window.blit(pic, stud_green_rect)
+            pygame.draw.rect(self.window, red, stud_green_rect, 2)
+
+        
+
+    # Проверяем, находится ли охранник рядом со студентом, который не является вором, если да, то количество жизней охранника уменьшается
+    def hittest(self, security):
+        if self.touch:
+            if self.minus_life:
+                security.live -= 1
+                self.minus_life = 0
+        else:
+            self.minus_life = 1
+
+        return False
+
+
+
+class Thief(Student):
+    def __init__(self, window: pygame.Surface):
+        super().__init__(window)
+        self.color = red
+        self.money = 0
+        self.number_table = 0
+        self.pay_time = 0
+
+    def draw(self):
+        if self.direction == 'r':
+            angle = 0
+        elif self.direction == 'l':
+            angle = 180
+        elif self.direction == 'd':
+            angle = 270
+        elif self.direction == 'u':
+            angle = 90
+        if self.state == 0:
+            pic = pygame.transform.rotate(stud_blue, angle)
+            stud_blue_rect.center = self.x, self.y
+            self.window.blit(pic, stud_blue_rect)
+        else:
+            pic = pygame.transform.rotate(stud_red, angle)
+            stud_red_rect.center = self.x, self.y
+            self.window.blit(pic, stud_red_rect)
+            pygame.draw.rect(self.window, red, stud_red_rect, 2)
+
+
+
+    def hittest(self, security):
+        if self.touch:
+            return True
+        else:
+            return False
+
+    
+
